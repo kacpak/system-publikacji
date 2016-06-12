a4b90da8068f8b7a9061cc4227f464a49d4455c6
Mateusz Kasprzak <kacpak@gmail.com>
2016-06-12 11:10:16 +0200

    Added refresh capability to publications list

--- js/controllers/publicationList.js
+++ js/controllers/publicationList.js
@@ -3,9 +3,19 @@
     function PublicationListController($scope, publicationData) {
         $scope.sortType = 'title';
         $scope.sortReverse = false;
-        publicationData.getPublications(function success(data) {
-            $scope.data = data;
-        });
+        $scope.resfreshInProgress = false;
+
+        $scope.refresh = function () {
+            $scope.resfreshInProgress = true;
+            publicationData.getPublications(function success(data) {
+                $scope.data = data;
+                $scope.resfreshInProgress = false;
+            }, function error() {
+                $scope.resfreshInProgress = false;
+            });
+        }
+
+        $scope.refresh();
     }
 
     angular.module('app').controller('PublicationListController', PublicationListController);
